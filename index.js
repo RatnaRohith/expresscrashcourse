1593e276d29af8dbe3e06f005ea351f2176a6b29
Rohith Boddukuri <Tony Boddukuri>
2020-02-22 22:01:26 +0530

    Added js files for express

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const path = require('path');
+const members = require('./Members')
+const logger = require('./middleware/logger');
 
 const app = express();
 
-app.get('/',(req,res) => {
-    res.sendFile(path.join(__dirname, 'public','index.html'));
+
+
+// app.use(logger); 
+
+app.get('/api/members', (req,res) => {
+    res.json(members);
 });
+
+//Get  single member
+app.get('/api/members/:id',(req,res) => {
+    const found = members.some(member => member.id === parseInt(req.params.id));
+    
+    res.json(members.filter(member => member.id === parseInt(req.params.id)));
+    // res.send(req.params.id);
+});
+
+//Set static folder
+app.use(express.static(path.join(__dirname,'public')));
+// app.get('/',(req,res) => {
+//     res.sendFile(path.join(__dirname, 'public','index.html'));
+// });
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`));
